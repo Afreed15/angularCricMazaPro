9c5a1b71126b804b72b19e9f1679c76e2627f41f
Afreed15 <Afreed.vs786@gmail.com>
2021-04-05 10:37:35 +0530

    components Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlayersProfileComponent } from './players-profile/players-profile.component';
+import { PlayersComponent } from './players/players.component';
+import { PointsTableComponent } from './points-table/points-table.component';
+import { TeamsComponent } from './teams/teams.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'teams',component:TeamsComponent},
+  {path:'players',component:PlayersComponent},
+  {path:'points-table',component:PointsTableComponent},
+  {path:'players-profile',component:PlayersProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TeamsComponent } from './teams/teams.component';
-import { PlayersComponent } from './Players/players/players.component';
+import { PlayersComponent } from './players/players.component'
+import { PointsTableComponent } from './points-table/points-table.component';
+import { PlayersProfileComponent } from './players-profile/players-profile.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TeamsComponent,
-    PlayersComponent
+    PlayersComponent,
+    PointsTableComponent,
+    PlayersProfileComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
